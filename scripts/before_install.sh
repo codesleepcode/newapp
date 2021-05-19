7cd1c3e14a575defcebe906ef5426fef4094cf9d
codesleepcode <siddhartha_97@outlook.com>
2021-05-19 21:44:21 +1000

    ch

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,11 +6,10 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 #create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
+DIR="/home/ec2-user/mynodeweb"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
-© 2021 GitHub, Inc.
