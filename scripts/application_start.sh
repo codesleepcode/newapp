e321426d2a8fe4c10fc570cfb22540cfc6f4a604
codesleepcode <siddhartha_97@outlook.com>
2021-05-19 20:58:18 +1000

    changed app_Start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+sudo chmod -R 777 /home/ec2-user/mynodeweb
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ec2-user/mynodeweb
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
