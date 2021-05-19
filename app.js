d57e954e08dcec39c958b0b85862d0de6d74daa7
codesleepcode <siddhartha_97@outlook.com>
2021-05-19 22:07:29 +1000

    j

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
 
 app.get('/', (req,res) => {
-    res.send("<h1>welcome to my app</h1>");
+    res.send("<html><h1>welcome to my app</h1></html>");
 })
 
-app.listen(port, ()=>{
-    console.log(`listening on port:${port}`);
+app.listen(3000, ()=>{
+    console.log(`listening....`);
 })
 
